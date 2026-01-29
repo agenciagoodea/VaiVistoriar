6ab5bee9f6002e3d95a5a17424b0c6890092928f
agenciagoodea <agenciagoodea@gmail.com>
2026-01-29 19:45:03 -0400

    v4.9 Configurando Conexão com Supabase (VistoriaPro10)

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://cmrgzaoexmjilvbuduek.supabase.co';
-const supabaseAnonKey = 'sb_publishable_jD3NgKax7-Hji9-5zvUWGw_2KdanWcU';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
