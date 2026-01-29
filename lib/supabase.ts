3db903a3958f3467e2b463881faaa447d6f6de71
agenciagoodea <agenciagoodea@gmail.com>
2026-01-29 19:51:22 -0400

    v4.9 Supabase

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://cmrgzaoexmjilvbuduek.supabase.co';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'sb_publishable_jD3NgKax7-Hji9-5zvUWGw_2KdanWcU';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
